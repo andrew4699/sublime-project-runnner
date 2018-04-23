b491fc6fe4a4deecd984d9e51a9555a6216d3e62
Andrew Guterman <andrew_guterman@yahoo.com>
2018-04-22 17:24:33 -0700

    started on stopping

--- projectrunner.py
+++ projectrunner.py
@@ -14,20 +14,31 @@ class ProjectRunner(sublime_plugin.EventListener):
 
 	def on_activated(self, view):
 		win = view.window()
-		
+		#print(1)
 		if win is not None:
+			#print(2)
 			project_file = win.project_file_name()
 			
 			if project_file is not None:
 				project_data = win.project_data()
 
-				if "projectrunner" in project_data and project_file != self.project_file:
-					self.project_file = project_file
-					self.project_data = project_data['projectrunner']
-					self.window = win
-					self.view = view
-					self.process_project_data()
+				if project_file != self.project_file:
+					print("switched project - " + project_file)
+					if self.project_file is not None:
+						self.stop()
 
-	def process_project_data(self):
+					if "projectrunner" in project_data:
+						print(">> do it")
+						self.project_file = project_file
+						self.project_data = project_data['projectrunner']
+						self.window = win
+						self.view = view
+						self.start()
+
+	def start(self):
 		if "start" in self.project_data and self.project_data['start'] != False:
 			self.start_handler = StartHandler(self)
+
+	def stop(self):
+		if True and self.start_handler is not None:
+			self.start_handler.stop()
--- starthandler.py
+++ starthandler.py
@@ -1,5 +1,6 @@
 import threading
 import os
+import subprocess
 
 from . import constants
 
@@ -33,5 +34,14 @@ class StartHandler():
 		else:
 			cmd = constants.DEFAULT_STARTUP
 
-		os.chdir(self.get_run_path());
-		self.project_runner.terminal = os.system("npm start");
+		#os.chdir(self.get_run_path());
+		#self.project_runner.terminal = os.system(cmd);
+		print("lets do it - " + self.get_run_path())
+		self.project_runner.terminal = subprocess.Popen("cmd.exe /K npm start", shell=True, cwd=self.get_run_path())
+		#self.project_runner.terminal = subprocess.call(cmd)
+
+	def stop(self):
+		if self.project_runner.terminal is not None:
+			print("stop")
+			#subprocess.Popen("TASKKILL /F /PID {pid} /T".format(pid = self.project_runner.terminal.pid))
+			self.project_runner.terminal = None
